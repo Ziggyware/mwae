c9318d1b489a2e2e8b2efa7371c215099a320d38
ziggyware <ziggyware@gmail.com>
2023-07-07 23:05:24 -0500

    Updated solidity version and imported OpenZeppelin contracts.

--- contracts/combined.sol
+++ contracts/combined.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity^0.8.0;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "/@openzeppelin/contracts/utils/Counters.sol";
+import "/@openzeppelin/contracts/access/Ownable.sol";
+import "/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "/@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 // Auction contract for managing auctions for each NFT
 contract Auction is Ownable, ReentrancyGuard {
