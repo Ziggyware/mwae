b79aa198eb55dd442a269375dcd3396a33f9e0b6
ziggyware <ziggyware@gmail.com>
2023-07-07 23:10:30 -0500

    aider: Implement the nft buy, sell, list, de-list functionality within contracts/combined.sol in src/App.js.
    # Context:
    USER: update src/App.js to implement the nft buy, sell, list, de-list functionality within contracts/combined.sol

--- src/App.js
+++ src/App.js
@@ -1,71 +1,142 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import { ZillennialMemeopolisABI } from './contracts/ZillennialMemeopolisABI'; // Import the ABI of the ZillennialMemeopolis contract
+import './App.css';
+
+// Import the contract ABI
+import CombinedContract from './contracts/Combined.json';
 
 function App() {
   const [contract, setContract] = useState(null);
+  const [account, setAccount] = useState('');
   const [tokenId, setTokenId] = useState('');
-  const [tokenPrice, setTokenPrice] = useState(0);
-  const [balance, setBalance] = useState(0);
-
+  const [tokenPrice, setTokenPrice] = useState('');
+  const [isListed, setIsListed] = useState(false);
 
   useEffect(() => {
-    initializeContract();
-  });
-  
+    // Function to initialize the contract
+    const initContract = async () => {
+      try {
+        // Connect to the Ethereum provider
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-  async function initializeContract() {
+        // Get the signer account
+        const signer = provider.getSigner();
+
+        // Get the network ID
+        const networkId = await provider.getNetwork().then((network) => network.chainId);
+
+        // Get the contract address from the contract JSON file
+        const contractAddress = CombinedContract.networks[networkId].address;
+
+        // Create a new contract instance
+        const combinedContract = new ethers.Contract(contractAddress, CombinedContract.abi, signer);
+
+        // Set the contract instance
+        setContract(combinedContract);
+
+        // Get the current account
+        const currentAccount = await signer.getAddress();
+
+        // Set the account
+        setAccount(currentAccount);
+      } catch (error) {
+        console.error('Error initializing contract:', error);
+      }
+    };
+
+    // Initialize the contract
+    initContract();
+  }, []);
+
+  // Function to handle buying an NFT
+  const handleBuyNFT = async () => {
     try {
-      // Connect to the Ethereum network
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send('eth_requestAccounts', []);
-      
-      // Get the signer
-      const signer = provider.getSigner();
+      // Call the buy function in the contract
+      await contract.buy(tokenId, { value: tokenPrice });
 
-      // Create an instance of the ZillennialMemeopolis contract
-      const contractAddress = 'CONTRACT_ADDRESS'; // Replace with the actual contract address
-      const zillennialMemeopolisContract = new ethers.Contract(contractAddress, ZillennialMemeopolisABI, signer);
-
-      // Set the contract instance
-      setContract(zillennialMemeopolisContract);
-
-      // Get the token price
-      const price = await zillennialMemeopolisContract.getTokenPrice(tokenId);
-      setTokenPrice(price);
-
-      // Get the contract balance
-      const contractBalance = await provider.getBalance(contractAddress);
-      setBalance(ethers.utils.formatEther(contractBalance));
+      // Refresh the token details
+      await getTokenDetails();
     } catch (error) {
-      console.error('Error initializing contract:', error);
+      console.error('Error buying NFT:', error);
     }
-  }
+  };
 
-  async function handleMintNFT() {
+  // Function to handle selling an NFT
+  const handleSellNFT = async () => {
     try {
-      // Mint a new NFT
-      const recipient = 'RECIPIENT_ADDRESS'; // Replace with the actual recipient address
-      const tokenURI = 'TOKEN_URI'; // Replace with the actual token URI
-      const metadataURI = 'METADATA_URI'; // Replace with the actual metadata URI
-      await contract.mintNFT(recipient, tokenURI, metadataURI);
+      // Call the sell function in the contract
+      await contract.sell(tokenId, tokenPrice);
 
-      // Refresh the contract balance
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contractBalance = await provider.getBalance(contract.address);
-      setBalance(ethers.utils.formatEther(contractBalance));
+      // Refresh the token details
+      await getTokenDetails();
     } catch (error) {
-      console.error('Error minting NFT:', error);
+      console.error('Error selling NFT:', error);
     }
-  }
+  };
+
+  // Function to handle listing an NFT
+  const handleListNFT = async () => {
+    try {
+      // Call the list function in the contract
+      await contract.list(tokenId, tokenPrice);
+
+      // Refresh the token details
+      await getTokenDetails();
+    } catch (error) {
+      console.error('Error listing NFT:', error);
+    }
+  };
+
+  // Function to handle de-listing an NFT
+  const handleDelistNFT = async () => {
+    try {
+      // Call the delist function in the contract
+      await contract.delist(tokenId);
+
+      // Refresh the token details
+      await getTokenDetails();
+    } catch (error) {
+      console.error('Error delisting NFT:', error);
+    }
+  };
+
+  // Function to get the details of the token
+  const getTokenDetails = async () => {
+    try {
+      // Call the getTokenPrice function in the contract
+      const price = await contract.getTokenPrice(tokenId);
+
+      // Call the getTokenListingStatus function in the contract
+      const listed = await contract.getTokenListingStatus(tokenId);
+
+      // Set the token price and listing status
+      setTokenPrice(price.toString());
+      setIsListed(listed);
+    } catch (error) {
+      console.error('Error getting token details:', error);
+    }
+  };
 
   return (
-    <div>
-      <h1>Zillennial Memeopolis</h1>
-      <p>Token ID: {tokenId}</p>
-      <p>Token Price: {tokenPrice} ETH</p>
-      <p>Contract Balance: {balance} ETH</p>
-      <button onClick={handleMintNFT}>Mint NFT</button>
+    <div className="App">
+      <h1>NFT Marketplace</h1>
+      <div>
+        <label>Token ID:</label>
+        <input type="text" value={tokenId} onChange={(e) => setTokenId(e.target.value)} />
+      </div>
+      <div>
+        <label>Token Price:</label>
+        <input type="text" value={tokenPrice} onChange={(e) => setTokenPrice(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={handleBuyNFT}>Buy NFT</button>
+        <button onClick={handleSellNFT}>Sell NFT</button>
+        {isListed ? (
+          <button onClick={handleDelistNFT}>De-list NFT</button>
+        ) : (
+          <button onClick={handleListNFT}>List NFT</button>
+        )}
+      </div>
     </div>
   );
 }
