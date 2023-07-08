17af7771275c14c305515e54bfaeeb5873d2eae1
ziggyware <ziggyware@gmail.com>
2023-07-07 23:06:29 -0500

    aider: Update src/App.js to implement the NFT functionality within contracts/combined.sol.
    # Context:
    USER: update src/App.js to implement the nft functionality within contracts/combined.sol

--- src/App.js
+++ src/App.js
@@ -1,49 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import CombinedContract from './contracts/CombinedContract.json';
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { ZillennialMemeopolisABI } from './contracts/ZillennialMemeopolisABI'; // Import the ABI of the ZillennialMemeopolis contract
 
 function App() {
   const [contract, setContract] = useState(null);
-  const [data, setData] = useState('');
+  const [tokenId, setTokenId] = useState('');
+  const [tokenPrice, setTokenPrice] = useState(0);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
-    const init = async () => {
-      try {
-        // Connect to the blockchain
-        const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-        const networkId = await web3.eth.net.getId();
-
-        // Get the contract instance
-        const deployedNetwork = CombinedContract.networks[networkId];
-        const contractInstance = new web3.eth.Contract(
-          CombinedContract.abi,
-          deployedNetwork && deployedNetwork.address
-        );
-
-        setContract(contractInstance);
-      } catch (error) {
-        console.error('Error initializing contract:', error);
-      }
-    };
-
-    init();
+    initializeContract();
   }, []);
 
-  const fetchData = async () => {
+  async function initializeContract() {
     try {
-      // Call the contract function to fetch data
-      const result = await contract.methods.getData().call();
-      setData(result);
+      // Connect to the Ethereum network
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send('eth_requestAccounts', []);
+      
+      // Get the signer
+      const signer = provider.getSigner();
+
+      // Create an instance of the ZillennialMemeopolis contract
+      const contractAddress = 'CONTRACT_ADDRESS'; // Replace with the actual contract address
+      const zillennialMemeopolisContract = new ethers.Contract(contractAddress, ZillennialMemeopolisABI, signer);
+
+      // Set the contract instance
+      setContract(zillennialMemeopolisContract);
+
+      // Get the token price
+      const price = await zillennialMemeopolisContract.getTokenPrice(tokenId);
+      setTokenPrice(price);
+
+      // Get the contract balance
+      const contractBalance = await provider.getBalance(contractAddress);
+      setBalance(ethers.utils.formatEther(contractBalance));
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error initializing contract:', error);
     }
-  };
+  }
+
+  async function handleMintNFT() {
+    try {
+      // Mint a new NFT
+      const recipient = 'RECIPIENT_ADDRESS'; // Replace with the actual recipient address
+      const tokenURI = 'TOKEN_URI'; // Replace with the actual token URI
+      const metadataURI = 'METADATA_URI'; // Replace with the actual metadata URI
+      await contract.mintNFT(recipient, tokenURI, metadataURI);
+
+      // Refresh the contract balance
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contractBalance = await provider.getBalance(contract.address);
+      setBalance(ethers.utils.formatEther(contractBalance));
+    } catch (error) {
+      console.error('Error minting NFT:', error);
+    }
+  }
 
   return (
     <div>
-      <h1>Combined Contract Data</h1>
-      <button onClick={fetchData}>Fetch Data</button>
-      <p>Data: {data}</p>
+      <h1>Zillennial Memeopolis</h1>
+      <p>Token ID: {tokenId}</p>
+      <p>Token Price: {tokenPrice} ETH</p>
+      <p>Contract Balance: {balance} ETH</p>
+      <button onClick={handleMintNFT}>Mint NFT</button>
     </div>
   );
 }
