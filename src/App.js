6320d50dc2bed2e9f0e1daf1c646b12b62780409
ziggyware <ziggyware@gmail.com>
2023-07-07 22:55:10 -0500

    aider: Update `src/App.js` and `src/index.js` to implement the functionality from `contracts/combined.sol`.
    # Context:
    USER: update src\App.js and src\index.js to implement the functionality from `contracts\combined.sol`

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import CombinedContract from './contracts/CombinedContract.json';
 
 function App() {
+  const [contract, setContract] = useState(null);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        // Connect to the blockchain
+        const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+        const networkId = await web3.eth.net.getId();
+
+        // Get the contract instance
+        const deployedNetwork = CombinedContract.networks[networkId];
+        const contractInstance = new web3.eth.Contract(
+          CombinedContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+
+        setContract(contractInstance);
+      } catch (error) {
+        console.error('Error initializing contract:', error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      // Call the contract function to fetch data
+      const result = await contract.methods.getData().call();
+      setData(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Combined Contract Data</h1>
+      <button onClick={fetchData}>Fetch Data</button>
+      <p>Data: {data}</p>
     </div>
   );
 }
